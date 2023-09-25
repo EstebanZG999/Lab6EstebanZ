da58ad3891a250d216c56c5d0230c0dafffbcacd
EstebanZG999 <zam22119@uvg.edu.gt>
2023-09-24 18:39:57 -0600

    Cleaned

--- app/src/main/java/com/example/lab6ez/Screens/FavoritosScreen.kt
+++ app/src/main/java/com/example/lab6ez/Screens/FavoritosScreen.kt
@@ -12,24 +12,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.lab6ez.Navigation.AppNavigation
-import com.example.lab6ez.Navigation.TabScreens
 import com.example.lab6ez.R
 import com.example.lab6ez.ui.theme.Lab6EZTheme
 
--- app/src/main/java/com/example/lab6ez/Screens/MainSreen.kt
+++ app/src/main/java/com/example/lab6ez/Screens/MainSreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
